c4d52b39b187cd168372411d893f447be74299a6
Mariam Omar <umarsamiya@gmail.com>
2018-03-14 11:32:03 +0300

    add changes

--- src/app/highlightthrough.directive.ts
+++ src/app/highlightthrough.directive.ts
@@ -5,6 +5,7 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class HighlightthroughDirective {
 
-  constructor(private elem: ElementRef) { }
-
+  constructor(private elem: ElementRef) {
+    this.elem.nativeElement.style.fontWeight = 'bold';
+  }
 }
