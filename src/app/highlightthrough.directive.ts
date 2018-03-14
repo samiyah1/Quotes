d0d555beea2bd8c41182becffa7b4eae70f12bc9
Mariam Omar <umarsamiya@gmail.com>
2018-03-14 11:45:50 +0300

    add changes

--- src/app/highlightthrough.directive.ts
+++ src/app/highlightthrough.directive.ts
@@ -1,11 +1,19 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightthrough]'
 })
 export class HighlightthroughDirective {
 
-  constructor(private elem: ElementRef) {
-    this.elem.nativeElement.style.fontWeight = 'bold';
+  constructor(private elem: ElementRef) {}
+  @HostListener('click') onClicks() {
+    this.fontweight('bold');
+  }
+
+  @HostListener('dblclick') onDoubleClicks() {
+    this.fontweight('None');
+  }
+  private fontweight(action: string) {
+    this.elem.nativeElement.style.fontWeight = action;
   }
 }
