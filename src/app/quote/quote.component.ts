4827106b9128da0cec2a612647244a8d8348a9c4
Mariam Omar <umarsamiya@gmail.com>
2018-03-14 11:20:43 +0300

    add changes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,13 +7,16 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-    new Quote(1, 'Watch Finding Nemo','Find an online version and watch merlin find his son'),
+    new Quote(1, 'Watch Finding Nemo', 'Find an online version and watch merlin find his son'),
     new Quote(2, 'Buy Cookies', 'I have to buy cookies for the parrot'),
-    new Quote(3, 'Get new Phone Case','Diana has her birthday coming up soon'),
-    new Quote(4, 'Get Dog Food',' Pupper likes expensive sancks'),
-    new Quote(5, 'Solve math homework','Damn Math'),
-    new Quote(6, 'Plot my world domination plan','Cause I am an evil overlord'),
+    new Quote(3, 'Get new Phone Case', 'Diana has her birthday coming up soon'),
+    new Quote(4, 'Get Dog Food', ' Pupper likes expensive sancks'),
+    new Quote(5, 'Solve math homework', ' Damn Math'),
+    new Quote(6, 'Plot my world domination plan', 'Cause I am an evil overlord'),
 ];
+showDetails(index) {
+  this.quotes[index].showDescription = !this.quotes[index].showDescription;
+}
   constructor() { }
 
   ngOnInit() {
