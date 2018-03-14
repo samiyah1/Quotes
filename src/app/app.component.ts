3b28823dbed3f048fb34be6e707c5baf29f30ffb
Mariam Omar <umarsamiya@gmail.com>
2018-03-14 10:50:01 +0300

    add changes to the new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,5 @@ import { Quote } from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-quotes = [
-    new Quote (1, 'Watch Finding Nemo'),
-    new Quote (2, 'Buy Cookies'),
-    new Quote (3, 'Get new Phone Case'),
-    new Quote (4, 'Get Dog Food'),
-    new Quote (5, 'Solve math homework'),
-    new Quote (6, 'Plot my world domination plan'),
 
-];
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes = [
+    new Quote (1, 'Watch Finding Nemo'),
+    new Quote (2, 'Buy Cookies'),
+    new Quote (3, 'Get new Phone Case'),
+    new Quote (4, 'Get Dog Food'),
+    new Quote (5, 'Solve math homework'),
+    new Quote (6, 'Plot my world domination plan'),
 
+];
   constructor() { }
 
   ngOnInit() {
